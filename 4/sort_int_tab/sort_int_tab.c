11c61b5058aa0d5bcbcf9212a5324ab0447b38cc
osavytsk <33601681+osavytsk@users.noreply.github.com>
2018-01-18 01:10:07 +0200

    Add files via upload

---
+++ 4/sort_int_tab/sort_int_tab.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_int_tab.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/13 11:25:46 by exam              #+#    #+#             */
+/*   Updated: 2016/12/13 11:36:17 by exam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	sort_int_tab(int *tab, unsigned int size)
+{
+	unsigned int	i;
+	int				temp;
+
+	i = 0;
+	temp = 0;
+	if (!tab || size == 0 || size == 1)
+		return ;
+	while (i < size - 1)
+	{
+		while (i < size - 1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				temp = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = temp;
+			}
+			i++;
+		}
+		size--;
+		i = 0;
+	}
+}
